3facb6cc4af808bd706ceb8c6a4b62a0b7811161
taeyoung <119taeyoung@naver.com>
2021-01-30 20:49:59 +0900

    #0.4 Getting to know Expo

--- app/App.js
+++ app/App.js
@@ -2,18 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-/**
- * In iOS and Android, the ways that make a mobile app like Natvie
- * 1. real native: Swift or objective-c -> Xcode / Java or Kotlin -> Android Studio
- * 2. webview based on app
- */
-
-/* npm run ios */
-// live reload: save -> refresh automatically
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello!!!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
