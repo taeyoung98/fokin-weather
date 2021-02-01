f05f324d109f630d3aa32798b17eb6720323324b
taeyoung <119taeyoung@naver.com>
2021-02-01 15:42:11 +0900

    #1.1 Loading Screen

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Loading from './Loading';
 
 /**
  * In iOS and Android, the ways that make a mobile app like Natvie
@@ -12,31 +13,6 @@ import { StyleSheet, Text, View } from 'react-native';
 // live reload: save -> refresh automatically
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.yellowView}></View>
-      <View style={{flex: 4, backgroundColor: 'blue'}}></View>
-      {/* <StatusBar style="auto" /> */}
-    </View>
+    <Loading />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,  // all space is used.  
-    backgroundColor: 'skyblue',
-    // native flexDirection default 'column'
-    // web flex-direction default 'row'
-    // flexDirection: 'row',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-  text: {
-    flex: 1,
-    color: 'white',
-    fontSize: 100
-  },
-  yellowView: {
-    flex: 1,
-    backgroundColor: 'yellow'
-  }
-});
---
+++ Loading.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+function Loading() {
+    return(
+        <View style={styles.contatiner}>
+            <Text style={styles.text}>Getting the Happy Weather</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    contatiner: {
+        flex: 1,
+        backgroundColor: '#FDF6AA',
+        justifyContent: 'flex-end',
+        paddingHorizontal: 30,
+        paddingVertical:100,
+    },
+    text: {
+        color: '#2c2c2c',
+        fontSize: 30,
+        width: '80%',
+    }
+})
+
+export default Loading;
