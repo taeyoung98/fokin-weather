c2ad2e4930ab43bf90bbe48d4369c7dd834f6656
taeyoung <119taeyoung@naver.com>
2021-02-01 17:00:15 +0900

    #1.3 Asking for Permissions

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 import Loading from './Loading';
 import * as Location from 'expo-location';
 
@@ -13,14 +13,32 @@ export default function App() {
 */
 
 export default class extends React.Component {
-  getLocation = async() => {
-    const location = await Location.getCurrentPositionAsync(options);
-    console.log(location);
+  state = {
+    isLoading: true
   }
+
+  getLocation = async() => {
+    try {
+      const { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+      const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
+      // Send to API and get weather
+      console.log(latitude, longitude);
+      // this.setState({isLoading: false});
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  }
+
   componentDidMount(){
     this.getLocation();
   }
+
   render() {
-    return <Loading />;
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
